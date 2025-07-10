435da9e5d5e9f1dd4ec488e479c040878411d7db
Rafael Mehdiyev <rafaelmehdiyev0@gmail.com>
2025-07-10 07:51:45 +0400

    Creation of the HomeScreen component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -729,7 +729,11 @@ export default function HomeScreen() {
         </View>
 
         <View testID="categoryList">
-       
+          <Categories
+            categories={categories}
+            activeCategory={activeCategory}
+            onChangeCategory={handleChangeCategory}
+          />
         </View>
 
         <View testID="foodList">
